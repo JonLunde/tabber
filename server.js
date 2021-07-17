2f163ca5f452d30dd418623ca472ca3fb228afec
JonMLunde <jonlund95@gmail.com>
2021-07-18 01:55:15 +0200

    a

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
 const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// Sets server up to listen to chosen port.
+app.listen(port, () => console.log(`Server listening on port ${port}`));
 
-// create a GET route
-app.get('/api/test', (req, res) => {
-  const customers = [
-    {
-    firstName: "jon",
-    lastName: "lunde",
-  },
-    {
-    firstName: "henrik",
-    lastName: "lunde",
-  },
-];
-  res.json(customers);
+// MIDDLEWARE
+// Handle CORS.
+app.use(cors());
+// Handle urlEncoded requests.
+app.use(express.urlencoded({ extended: false }));
+// Handle JSON requests.
+app.use(express.json());
+
+// ENDPOINTS
+app.post('/export', (req, res) => {
+  const textTab = req.body;
+  const writeTextTab = require('./writeTextTab');
+  writeTextTab(textTab);
+
+  res.status(201).json('Text file created.');
+});
+
+app.get('/export', (req, res) => {
+  const file = `${__dirname}/tmp/test.txt`;
+
+  res.status(200).download(file, 'test.txt', (err) => {
+    if (err) {
+      res.status(500).send({ message: err });
+    }
+    fs.unlinkSync(file);
+  });
 });
---
+++ writeTextTab.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+
+const createDataString = (textTab) => {
+  console.log('create: ', textTab);
+  let dataString = '';
+  dataString += textTab.tabDetails.title + ' - ';
+  dataString += textTab.tabDetails.artist + '\n\n';
+  if (textTab.tuning.name !== null) {
+    dataString += textTab.tuning.name + '\n';
+  }
+  if (textTab.tabDetails.capo !== '') {
+    dataString += textTab.tabDetails.capo + '\n';
+  }
+  if (textTab.tabDetails.source !== '') {
+    dataString += textTab.tabDetails.source + '\n\n';
+  } else dataString += '\n';
+
+  textTab.alignedTabs.forEach((tabBar) => {
+    dataString += tabBar.title + '\n';
+    tabBar.tabLines.forEach((line) => {
+      dataString += line + '\n';
+    });
+    dataString += '\n';
+  });
+
+  dataString = addNotationLegend(dataString);
+
+  return dataString;
+};
+
+const addNotationLegend = (dataString) => {
+  dataString += '\nh - Hammer on\n';
+  dataString += 'p - Pull off\n';
+  dataString += '/ - Slide up\n';
+  dataString += '\\ - Slide down\n';
+  dataString += 'X - Percussive slap\n';
+  dataString += '~ - Vibrato\n';
+  dataString += '+ - Hermonic\n';
+  dataString += 'x - Mute string\n';
+
+  return dataString;
+};
+
+const writeTextTab = (textTab) => {
+  console.log('write: ', textTab);
+  const dataString = createDataString(textTab);
+  console.log('DATASTRING: ', dataString);
+  fs.writeFile('./tmp/test.txt', dataString, (err) => {
+    if (err) return console.log(err);
+  });
+};
+
+module.exports = writeTextTab;
