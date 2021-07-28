1980f551ed5fa7ca4b1cb5061172f68280021ea4
JonMLunde <jonlund95@gmail.com>
2021-07-28 14:15:24 +0200

    trying to get heroku to work..

--- server.js
+++ server.js
@@ -5,16 +5,47 @@ const fs = require('fs');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// Heroku CORS options.
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:5000',
+  'http://localhost:8080',
+  'https://guitar-tabber.herokuapp.com/',
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log('** Origin of request ' + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log('Origin acceptable');
+      callback(null, true);
+    } else {
+      console.log('Origin rejected');
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
 // Sets server up to listen to chosen port.
 app.listen(port, () => console.log(`Server listening on port ${port}`));
 
 // MIDDLEWARE
 // Handle CORS.
-app.use(cors());
+app.use(cors(corsOptions));
 // Handle urlEncoded requests.
 app.use(express.urlencoded({ extended: false }));
 // Handle JSON requests.
 app.use(express.json());
+
+// For Heroku, makes sure the react client instead of backend.
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 // ENDPOINTS
 app.post('/export', (req, res) => {
